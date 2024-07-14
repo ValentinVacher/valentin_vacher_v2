e51c515535985fc915c04020fae2ea93210deffe
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-07-14 11:40:33 +0200

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from "./components/nav/nav.component";
+import {FwBackgroundComponent} from "./components/fw-background/fw-background.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FwBackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

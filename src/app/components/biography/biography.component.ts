cf734ec65de53b8f8e879053a3ce1233915ea0d6
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-07-19 11:29:21 +0200

    biography update

--- src/app/components/biography/biography.component.ts
+++ src/app/components/biography/biography.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {faUser, faCompass, faPhone} from "@fortawesome/free-solid-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-biography',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './biography.component.html',
   styleUrl: './biography.component.css'
 })
 export class BiographyComponent {
+  protected readonly faUser = faUser;
+  protected readonly faCompass = faCompass;
+  protected readonly faPhone = faPhone;
+  protected age = Math.abs(new Date(Date.now() - new Date(2001, 3, 13).getTime()).getUTCFullYear() - 1970);
 
 }

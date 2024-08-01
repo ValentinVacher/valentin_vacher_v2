60ce95159a50580e895f01345398466f95d6fcf4
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-08-01 10:42:37 +0200

    new biography

--- src/app/components/biography/biography.component.ts
+++ src/app/components/biography/biography.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {faUser, faCompass, faPhone} from "@fortawesome/free-solid-svg-icons";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {BiographyService} from "../../services/biography.service";
+import {Biography} from "../../models/biography.model";
 
 @Component({
   selector: 'app-biography',
@@ -16,11 +17,14 @@ export class BiographyComponent implements OnInit{
   protected readonly faUser = faUser;
   protected readonly faCompass = faCompass;
   protected readonly faPhone = faPhone;
-  protected age = Math.abs(new Date(Date.now() - new Date(2001, 3, 13).getTime()).getUTCFullYear() - 1970);
+  protected readonly age = Math.abs(new Date(Date.now() - new Date(2001, 3, 13).getTime()).getUTCFullYear() - 1970);
+  protected biography!: Biography;
 
   constructor(private biographyService: BiographyService) {}
 
   ngOnInit() {
-    console.log(this.biographyService.getBiography());
+    this.biographyService.getBiography().then(biography => {
+      this.biography = biography
+    });
   }
 }

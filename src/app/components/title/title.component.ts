3eaa7156a41357f00b55c2e27a336d0456390b8f
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-08-06 11:37:04 +0200

    responsive update

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-title',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './title.component.html',
   styleUrl: './title.component.css'
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
+  protected smaller = false;
+  protected highter = false
 
+  constructor(private breakpointObserver$: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver$.observe('(min-height: 720px)').subscribe(result => {
+      this.smaller = !result.matches
+      console.log("test")
+    });
+
+    this.breakpointObserver$.observe('(max-height: 850px)').subscribe(result => {
+      this.highter = !result.matches
+    });
+  }
 }

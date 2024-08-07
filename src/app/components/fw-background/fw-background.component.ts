9d1a2f9c26c90e3c1e0f03e538e3e521e018c32a
Valentin Vacher <108465376+ValentinVacher@users.noreply.github.com>
2024-08-07 12:53:28 +0200

    responsive update

--- src/app/components/fw-background/fw-background.component.ts
+++ src/app/components/fw-background/fw-background.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {BiographyComponent} from "../biography/biography.component";
+import {CareerComponent} from "../career/career.component";
 
 @Component({
   selector: 'app-fw-background',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    BiographyComponent
+    BiographyComponent,
+    CareerComponent
   ],
   templateUrl: './fw-background.component.html',
   styleUrl: './fw-background.component.css'
